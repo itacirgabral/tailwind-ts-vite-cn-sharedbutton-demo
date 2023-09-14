749dbb2bb6c18a838d5350156184c2cf7b7c06be
Itacir Gabral <itacirgabral@gmail.com>
2023-09-14 10:49:42 -0400

    multiple button

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import Button from '@/Button'
 function App() {
   return (
     <>
-      <Button>olá mundo</Button>
+      <Button className='m-2' primary>olá mundo</Button>
+      <Button className='m-2' secondary>olá mundo</Button>
+      <br />
+      <Button className='m-2' primary outline>olá mundo</Button>
+      <Button className='m-2' secondary outline onClick={() => console.log('secondary outline')}>olá mundo</Button>
     </>
   )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,11 +8,19 @@ import cn from '$/cn'
 
 interface ButtonProps extends PropsOf<"button"> {
   primary?: boolean;
+  secondary?: boolean;
+  outline?: boolean;
 }
 
-export default function Button ({ primary, className, children, ...props }: ButtonProps ) {
-  const defaulClass = 'px-3 py-2 border border-blue-600 bg-blue-500 text-white'
-  return <button {...props} className={cn(defaulClass)}>
+export default function Button ({ primary, secondary, outline, className, children, ...props }: ButtonProps ) {
+  const classes = cn('px-3 py-2 border rounded-full font-bold', {
+    'border-blue-800 bg-blue-400 text-white': primary,
+    'border-red-800 bg-red-400 text-white': secondary,
+    'bg-slate-200 text-blue-800': outline && primary,
+    'bg-slate-200 text-red-800': outline && secondary,
+  }, className)
+
+  return <button {...props} className={classes}>
     { children }
   </button>
 }
